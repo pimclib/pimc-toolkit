763c1c646c7d8474d7d28401b7d3e6b8b2e25457
pimclib <innokenty.rudenko@gmail.com>
2023-07-06 23:05:17 +0100

    in the process of adding PIM-SM Join/Prune config

---
+++ apps/pimc/Main.cpp
@@ -0,0 +1,7 @@
+#include <fmt/format.h>
+
+#include "config/JPConfig.hpp"
+
+int main(int argc, char** argv) {
+    return 0;
+}
---
+++ apps/pimc/config/JPConfig.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <optional>
+#include <vector>
+
+#include "pimc/net/IPv4Address.hpp"
+
+namespace pimc {
+
+class SharedTree final {
+public:
+    SharedTree(net::IPv4Address rp, std::vector<net::IPv4Address> prunes)
+    : rp_{rp}, prunes_{std::move(prunes)} {}
+
+    [[nodiscard]]
+    net::IPv4Address rp() const { return rp_; }
+
+    [[nodiscard]]
+    std::vector<net::IPv4Address> const& prunes() const {
+        return prunes_;
+    }
+private:
+
+private:
+    net::IPv4Address rp_;
+    std::vector<net::IPv4Address> prunes_;
+};
+
+class GroupEntry {
+public:
+    GroupEntry(
+            net::IPv4Address group,
+            std::optional<SharedTree> spt,
+            std::vector<net::IPv4Address> joins)
+            : group_{group}
+            , spt_{std::move(spt)}
+            , joins_{std::move(joins)} {}
+
+    [[nodiscard]]
+    net::IPv4Address group() const { return group_; }
+
+    [[nodiscard]]
+    std::optional<SharedTree> const& spt() const { return spt_; }
+
+    [[nodiscard]]
+    std::vector<net::IPv4Address> const& joins() const { return joins_; }
+private:
+    net::IPv4Address group_;
+    std::optional<SharedTree> spt_;
+    std::vector<net::IPv4Address> joins_;
+};
+
+class JPConfig final {
+public:
+    explicit JPConfig(std::vector<GroupEntry> groups)
+    : groups_{std::move(groups)} {}
+
+    [[nodiscard]]
+    std::vector<GroupEntry> const& groups() const { return groups_; }
+private:
+    std::vector<GroupEntry> groups_;
+};
+
+} // namespace pimc
